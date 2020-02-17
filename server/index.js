efb27fb331f9d084e7d8296189eaab3176cbdfee
Ibragim Abbasov <blancduman@gmail.com>
2020-02-18 01:59:19 +0400

    Revert "fix: change local.json data format; feat: add russian to first director."
    This reverts commit 88849199b230995286848d45aa4f0529061fc81b, reversing
    changes made to a81730f4b207e45e5c21a44c5e67fc6f5eb8075c.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 // const DB = JSON.parse(fs.readFileSync('./local.json'));
 const DB = Object.values(require('./local.json').directors);
 // const DBArray = Object.values(DB);
@@ -13,50 +10,17 @@ console.log(DB);
 console.log(DB[0]);
 // console.log(DBArray);
 
-app.get('/api/all_directors', (req, res) => {
+app.get('/all_directors', (req, res) => {
   res.json(DB);
 });
 
-app.get('/api/director/:id', (req, res) => {
+app.get('/director/:id', (req, res) => {
   const result = DB[req.params.id];
 
   res.json(result);
 });
-let counter = 0;
-app.get('/api/search', (req, res) => {
-  // var circ = {};
-  // circ.circ = circ;
-  // var cache = [];
-  // fs.writeFileSync(
-  //   `./tmp${counter}.json`,
-  //   JSON.stringify(req, function(key, value) {
-  //     if (typeof value === 'object' && value !== null) {
-  //       if (cache.indexOf(value) !== -1) {
-  //         // Duplicate reference found, discard key
-  //         return;
-  //       }
-  //       // Store value in our collection
-  //       cache.push(value);
-  //     }
-  //     return value;
-  //   }),
-  //   {
-  //     encoding: 'utf8',
-  //     mode: 2,
-  //     flag: 'a',
-  //   }
-  // );
-  // cache = null;
-  // counter++;
-  const query = req.query.query;
 
-  const result = DB.filter(i => i.nam);
-  res.json('hi');
-});
+app.get('/search', (req, res) => {});
 
-const port = 5000;
-const server = app.listen(port, () => {
-  console.log('Listening on port ' + server.address().port);
-});
-
-module.exports = server;
+// const result = Array.from(DB).filter(i => i.name === 'BigBoy');
+// console.log(result);
