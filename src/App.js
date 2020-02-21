572ff11ca322d111f7ca4c963d6527a1f69f183d
Ibragim Abbasov <blancduman@gmail.com>
2020-02-21 15:34:49 +0400

    fix: add example of use hooks in app; backend cors problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Router, Route, Switch, useParams } from 'react-router-dom';
+import { getDirector } from './apis/getData';
 import history from './history';
 
 function DummyComponent() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={DummyComponent} />
           <Route path="/search" exact component={DummyComponent} />
           <Route path="/all_directors" exact component={DummyComponent} />
+          <Route path="/director/:id" exact component={DummyComponent} />
           <Route path="/creators" exact component={DummyComponent} />
           <Route path="/styleguide" exact component={DummyComponent} />
           <Route path="/workflow" exact component={DummyComponent} />
@@ -24,3 +26,23 @@ function App() {
 }
 
 export default App;
+
+// function Dir() {
+//   const [director, setDirector] = useState(null);
+//   const { id } = useParams();
+//   useEffect(() => {
+//     (async directorId => {
+//       const result = await getDirector(directorId);
+//       setDirector(result);
+//     })(id);
+//   }, [id]);
+//   return director ? (
+//     <>
+//       <h1>{director.name.en}</h1>
+//       <h1>{director.birthPlace['en']}</h1>
+//       <h1>{director.lifetime}</h1>
+//     </>
+//   ) : (
+//     'loading'
+//   );
+// }
