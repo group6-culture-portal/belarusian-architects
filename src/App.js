54b98aed7dd18c27ee52c3a8d489cee05fd3ff2c
Andrei M <gagarin880120@gmail.com>
2020-02-21 19:29:44 +0300

    feat: add language context and text to mainPage.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from './history';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import Styleguide from './containers/styleguide/Styleguide';
 import MainPage from './containers/main_page/MainPage';
+import LanguageContext from './context/languageContext';
 import './index.css';
 
 function DummyComponent() {
@@ -12,19 +13,22 @@ function DummyComponent() {
 }
 
 function App() {
+  const [language, setLanguage] = useState('en');
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <Router history={history}>
-          <Switch>
-            <Route path="/" exact component={MainPage} />
-            <Route path="/search" exact component={DummyComponent} />
-            <Route path="/all_directors" exact component={DummyComponent} />
-            <Route path="/creators" exact component={DummyComponent} />
-            <Route path="/styleguide" exact component={Styleguide} />
-            <Route path="/workflow" exact component={DummyComponent} />
-          </Switch>
-        </Router>
+        <LanguageContext.Provider value={{ language: language, changeLanguage: setLanguage }}>
+          <Router history={history}>
+            <Switch>
+              <Route path="/" exact component={MainPage} />
+              <Route path="/search" exact component={DummyComponent} />
+              <Route path="/all_directors" exact component={DummyComponent} />
+              <Route path="/creators" exact component={DummyComponent} />
+              <Route path="/styleguide" exact component={Styleguide} />
+              <Route path="/workflow" exact component={DummyComponent} />
+            </Switch>
+          </Router>
+        </LanguageContext.Provider>
       </ThemeProvider>
     </div>
   );
--- src/containers/main_page/MainPage.js
+++ src/containers/main_page/MainPage.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { getDirector } from '../../apis/getData'
+import React, { useContext } from 'react';
+import languageContext from '../../context/languageContext';
+import { Typography } from '@material-ui/core/';
 
 export default function MainPage() {
-  const [director, setDirector] = useState(null);
-  const id = 3;
-  useEffect(() => {
-    (async directorId => {
-      const result = await getDirector(directorId);
-      setDirector(result);
-      console.log(result)
-    })(id);
-  }, [id]);
-  return director ? (
-        <>
-          <h1>{director.name.bl}</h1>
-          <h1>{director.lifetime}</h1>
-        </>
-      ) : (
-        'loading'
-      );
+  const { language } = useContext(languageContext);
+  return (
+    <Typography variant="body1">
+      {language === 'en' ?
+      `A 'theatre director' or stage director is a professional in the theatre field who oversees and orchestrates 
+      the mounting of a theatre production (a play, opera, musical, or devised piece of work) by unifying various 
+      endeavors and aspects of production. The director's function is to ensure the quality and completeness of 
+      theatre production and to lead the members of the creative team into realizing their artistic vision for it.
+      The director thereby collaborates with a team of creative individuals and other staff, coordinating research, 
+      stagecraft, costume design, props, lighting design, acting, set design, stage combat, and sound design for the production.` : 
+      language === 'ru' ?
+      `Театральный режиссер" или режиссер-постановщик - это профессионал в театральной области, который курирует и 
+      осуществляет постановку произведения (спектакля, оперы, мюзикла) путем объединения различных начинаний и аспектов работы. 
+      Функция режиссера заключается в обеспечении качества и полноты театральной постановки, а также в том, чтобы привести членов 
+      творческого коллектива к реализации их художественного видения спектакля. Таким образом, режиссер сотрудничает с командой 
+      творческих личностей и другими сотрудниками, координируя исследования, сценографию, дизайн костюмов, реквизит, 
+      световое оформление, актерское мастерство, эскиз декораций, сценический поединок и звуковой дизайн спектакля.` :
+      `bel`
+      }
+    </Typography>
+  )
 }
