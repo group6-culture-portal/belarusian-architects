811d6115bb45b677c535d6f56181c776e740ff77
Ibragim Abbasov <blancduman@gmail.com>
2020-02-23 14:57:56 +0400

    fix: change search to any language

--- server/index.js
+++ server/index.js
@@ -31,13 +31,16 @@ app.get('/api/director/:id', (req, res) => {
 });
 
 app.post('/api/search', (req, res) => {
-  const { query, lang } = req.body;
+  const { query } = req.body;
+  const comparing = query.toLowerCase();
 
-  const result = DB.filter(
-    i =>
-      i.name[lang].toLowerCase().includes(query.toLowerCase()) ||
-      i.birthPlace[lang].toLowerCase().includes(query.toLowerCase())
-  );
+  const result = DB.filter(i => {
+    const names = Object.values(i.name).join(' ');
+    const cities = Object.values(i.birthPlace).join(' ');
+
+    if (names.toLowerCase().includes(comparing) || cities.toLowerCase().includes(comparing))
+      return true;
+  });
 
   res.json(result);
 });
