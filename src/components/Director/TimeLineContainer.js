e70eeaa2704393a7a1647bd9e93ca25efb76b2d7
Ibragim Abbasov <blancduman@gmail.com>
2020-02-28 19:27:04 +0400

    styles: add colors for dates and background

--- src/components/Director/TimeLineContainer.js
+++ src/components/Director/TimeLineContainer.js
@@ -1,12 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
-import languageContext from '../../context/languageContext';
 import { Typography } from '@material-ui/core';
+
+import languageContext from '../../context/languageContext';
 import './Timeline.scss';
+
 function TimeLineList(items) {
   const result = items.map((item, index) => {
     return (
-      <TimelineItem key={index} dateText={item.dateText} style={{ color: '#e86971' }}>
+      <TimelineItem
+        key={index}
+        dateText={item.dateText}
+        style={{ color: '#e86971' }}
+        dateInnerStyle={{ background: '#373737', color: '#ffffff' }}
+        bodyContainerStyle={{
+          background: '#f5f5f5',
+          borderRadius: '8px',
+          boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+          padding: '1px 21px',
+          textAlign: 'justify',
+        }}
+      >
         <Typography variant="subtitle1" gutterBottom>
           {item.title}
         </Typography>
@@ -21,37 +35,8 @@ const TimeLineContainer = props => {
   const { biography } = props;
 
   const { language } = useContext(languageContext);
-  const [text, setText] = useState('');
 
-  useEffect(() => {
-    switch (language) {
-      case 'ru':
-        setText('Биография:');
-        break;
-
-      case 'bl':
-        setText('Біяграфія:');
-        break;
-
-      case 'en':
-        setText('Biography:');
-        break;
-
-      default:
-        break;
-    }
-  }, [language]);
-
-  return (
-    <>
-      <Typography variant="h4" style={{ textAlign: 'left', paddingLeft: 25 }}>
-        {text}
-      </Typography>
-      <Timeline lineColor={'#ddd'} style={{ width: '100%' }}>
-        {biography ? TimeLineList(biography[language]) : null}
-      </Timeline>
-    </>
-  );
+  return <Timeline lineColor={'#ddd'}>{biography && TimeLineList(biography[language])}</Timeline>;
 };
 
 export default TimeLineContainer;
