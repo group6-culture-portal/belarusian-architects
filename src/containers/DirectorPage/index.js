e03d4b39e532b925e1783effc43a80f0339916a1
Ibragim Abbasov <blancduman@gmail.com>
2020-02-28 19:30:11 +0400

    feat: add component for Director Page

--- src/containers/DirectorPage/index.js
+++ src/containers/DirectorPage/index.js
@@ -1,34 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { getDirector } from '../../apis/getData';
-import LanguageContext from '../../context/languageContext';
-import TimeLineContainer from '../../components/Director/TimeLineContainer';
-import './DirectorPage.scss';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '../../components/Director/Table';
-import { Grid, Typography } from '@material-ui/core';
-import Gallery from '../../components/Director/Gallery';
-import Gallery2 from '../../components/Director/Gallery2';
-import VideoGallery from '../../components/Director/VideoGallery';
-import VideoGallery2 from '../../components/Director/VideoGallery2';
+import { useParams } from 'react-router-dom';
 import { YMaps, Map, GeoObject } from 'react-yandex-maps';
+import { Grid, Typography } from '@material-ui/core';
+import Lightbox from 'react-lightbox-component';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    height: '100vh',
-  },
-  paper: {
-    // padding: theme.spacing(2),
-    backgroundColor: 'none',
-    textAlign: 'center',
-    // color: theme.palette.text.secondary,
-  },
-  table: {
-    minWidth: 500,
-  },
-}));
+import TimeLineContainer from '../../components/Director/TimeLineContainer';
+import Table from '../../components/Director/Table';
+import Gallery from '../../components/Director/Gallery';
+import VideoGallery from '../../components/Director/VideoGallery';
 
+import LanguageContext from '../../context/languageContext';
+import { getDirector } from '../../apis/getData';
+
+import './DirectorPage.scss';
 const Director = props => {
   const [director, setDirector] = useState(null);
 
@@ -39,15 +23,15 @@ const Director = props => {
   useEffect(() => {
     switch (language) {
       case 'ru':
-        setText('Театр:');
+        setText('Театр');
         break;
 
       case 'bl':
-        setText('Тэатр:');
+        setText('Тэатр');
         break;
 
       case 'en':
-        setText('Theater:');
+        setText('Theater');
         break;
 
       default:
@@ -61,7 +45,6 @@ const Director = props => {
       setDirector(result);
     })(id);
   }, [id]);
-  const classes = useStyles();
 
   if (director) {
     const [firstname, lastname] = director.name[language].split(' ');
@@ -69,26 +52,45 @@ const Director = props => {
       <div className="director_page">
         <main className="content">
           <Grid item xs>
-            {/* <Paper className={classes.paper}> */}
-            <Typography variant="h1">{director.name[language]}</Typography>
+            <Typography variant="h1" style={{ padding: '25px 0' }}>
+              {director.name[language]}
+            </Typography>
             <TimeLineContainer biography={director.biography} />
             <Table rows={director.works[language]} style={{ margin: '0 25px' }} />
-            {/* <Gallery photos={director.gallery} /> */}
-            <Gallery2 photos={director.gallery} />
-            {/* </Paper> */}
-            <Typography variant="h4" style={{ textAlign: 'left', paddingLeft: 25, marginTop: 25 }}>
-              {text}
-            </Typography>
-            <YMaps>
+            <Gallery photos={director.gallery} />
+            <div
+              style={{
+                backgroundColor: '#343434',
+                color: '#fff',
+                textAlign: 'center',
+                marginBottom: 10,
+                borderRadius: 4,
+              }}
+            >
+              <Typography
+                variant="h4"
+                style={{ textAlign: 'left', paddingLeft: 15, marginTop: 25 }}
+              >
+                {text}
+              </Typography>
+            </div>
+            <YMaps className="roundedBorders">
               <Map
-                width={724}
+                width={'90%'}
                 height={400}
+                style={{
+                  marginLeft: '5%',
+                  marginRight: '5%',
+                  height: 400,
+                }}
+                className="roundedBorders"
                 defaultState={{
                   center: [director.map.latitude, director.map.longitude],
                   zoom: 15,
                 }}
               >
                 <GeoObject
+                  className="roundedBorders"
                   geometry={{
                     type: 'Point',
                     coordinates: [director.map.latitude, director.map.longitude],
@@ -97,27 +99,39 @@ const Director = props => {
                 />
               </Map>
             </YMaps>
-            {/* {director.videos.map((video, index) => {
-              return <Video video={video} name={index} key={video + index} />;
-            })} */}
-            {/* <VideoGallery videos={director.videos} /> */}
-            <VideoGallery2 videos={director.videos} />
+            <VideoGallery videos={director.videos} />
           </Grid>
         </main>
         <aside className="more">
           <div className="director-profile">
-            <img src={director.photo} alt={''} />
+            <Lightbox
+              images={[
+                {
+                  src: director.photo,
+                  title: director.name[language],
+                  description: ' ',
+                },
+              ]}
+              renderImageFunc={(idx, image, toggleLightbox) => {
+                return (
+                  <img
+                    key={idx}
+                    className="makeCursor"
+                    src={image.src}
+                    alt={director.name[language]}
+                    style={{
+                      width: 270,
+                      height: 370,
+                      borderRadius: 8,
+                    }}
+                    onClick={toggleLightbox.bind(null, idx)}
+                  />
+                );
+              }}
+            />
             <Typography variant="h1">{`${lastname} ${firstname.charAt(0)}.`}</Typography>
             <Typography variant="subtitle1">{director.lifetime}</Typography>
             <Typography variant="body1">{director.summary[language]}</Typography>
-            <div className="another_directors">
-              <Link to={`/director/${director.prev.id}`}>
-                <Typography variant="body1">{director.prev.name[language]}</Typography>
-              </Link>
-              <Link to={`/director/${director.next.id}`}>
-                <Typography variant="body1">{director.next.name[language]}</Typography>
-              </Link>
-            </div>
           </div>
         </aside>
       </div>
@@ -127,3 +141,18 @@ const Director = props => {
 };
 
 export default Director;
+
+/* <div className="another_directors">
+              <Link to={`/director/${director.prev.id}`}>
+                <Typography variant="body1">
+                  <NavigateBefore fontSize="small" />
+                  {director.prev.name[language]}
+                </Typography>
+              </Link>
+              <Link to={`/director/${director.next.id}`}>
+                <Typography variant="body1">
+                  <NavigateNext fontSize="small" />
+                  {director.next.name[language]}
+                </Typography>
+              </Link>
+            </div> */
