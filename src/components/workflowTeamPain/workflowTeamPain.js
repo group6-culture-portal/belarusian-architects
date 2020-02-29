58e8c1bc4e5efd52547aefc44d1ff4cfed2b5817
Ibragim Abbasov <blancduman@gmail.com>
2020-02-29 18:18:49 +0400

    styles: correct layer sort

--- src/components/workflowTeamPain/workflowTeamPain.js
+++ src/components/workflowTeamPain/workflowTeamPain.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { getCreatorsPain } from '../../apis/getData'
+import { getCreatorsPain } from '../../apis/getData';
 
 import { List, ListItem, ListItemIcon, ListItemText, Icon } from '@material-ui/core/';
 import StarIcon from '@material-ui/icons/Star';
@@ -11,71 +11,74 @@ import { Typography } from '@material-ui/core/';
 function WorkflowTeamPain(props) {
   const { language } = useContext(languageContext);
 
-  let selectLanguage = (language) => {
+  let selectLanguage = language => {
     if (language === 'en') {
       return 0;
     } else if (language === 'ru') {
       return 1;
     } else if (language === 'bl') {
-      return 2
+      return 2;
     }
-  }
+  };
 
   const [teamPain, setTeamPain] = useState(null);
 
   useEffect(() => {
-    getCreatorsPain().then((res)=>{
-      setTeamPain(res)
-    })
-},[])
+    getCreatorsPain().then(res => {
+      setTeamPain(res);
+    });
+  }, []);
 
   const useStyles = makeStyles({
     header: {
       maxWidth: 1024,
-      textAlign: "center",
-      margin: "0 auto",
-      marginTop: 20
+      textAlign: 'center',
+      margin: '0 auto',
+      marginTop: 20,
     },
     h1: {
       fontSize: 40,
-      display: "block",
-    }
+      display: 'block',
+    },
   });
-  
+
   const classes = useStyles();
 
-  const getHeader = (language) => {
+  const getHeader = language => {
     if (language === 'ru') {
-      return "Сложности, с которыми столкнулась команда"
+      return 'Сложности, с которыми столкнулась команда';
     } else if (language === 'en') {
-      return "The difficulties faced by the team"
+      return 'The difficulties faced by the team';
     } else if (language === 'bl') {
-      return "Складанасці, з якімі сутыкнулася каманда"
+      return 'Складанасці, з якімі сутыкнулася каманда';
     }
-  }
-
+  };
 
   if (teamPain) {
-  return (
-    <React.Fragment>
-    <Typography variant="h2" style={{textAlign:"center", marginBottom: 20, fontSize: 32}}>{getHeader(language)}</Typography>
-    <List aria-label="pain">
-      {teamPain[selectLanguage(language)].map((element, index) => {
-        return (
-          <ListItem key={index} style={{alignItems: "center"}}>
-            <ListItemIcon style={{textAlign:"center", marginLeft: 20}}>
-            <Icon style={{ fontSize: 30 }}>add_circle</Icon>
-            </ListItemIcon>
-            <ListItemText primary={element} />
-          </ListItem>
-        )
-      })}
-    </List>
-    </React.Fragment>
-  )
+    return (
+      <React.Fragment>
+        <Typography variant="h2" style={{ textAlign: 'center', marginBottom: 20, fontSize: 32 }}>
+          {getHeader(language)}
+        </Typography>
+        <List aria-label="pain">
+          {teamPain[selectLanguage(language)].map((element, index) => {
+            return (
+              <ListItem key={index} style={{ alignItems: 'center' }}>
+                <ListItemIcon style={{ textAlign: 'center', marginLeft: 20 }}>
+                  <Icon style={{ fontSize: 30 }}>
+                    <StarIcon />
+                  </Icon>
+                </ListItemIcon>
+                <ListItemText primary={element} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </React.Fragment>
+    );
   } else {
-    return ('loading')
+    return 'loading';
   }
 }
 
-export default WorkflowTeamPain
+export default WorkflowTeamPain;
